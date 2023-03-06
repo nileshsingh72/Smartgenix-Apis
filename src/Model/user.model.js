aeb1c67ca8efc79783f6353edc8fbc1c602e167c
Faizan7012 <frtoons7012@gmail.com>
2023-03-06 16:31:04 +0530

    new

--- src/Model/user.model.js
+++ src/Model/user.model.js
@@ -3,7 +3,6 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     role: { type: String, default: "user" },
-    contact: { type: Number, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
   },
